62c9428128f8daac1f8b28199bad7e9117bad88a
Gustavo Kendy de Souza Kishima <gustavo.kishima@brasilparalelo.com.br>
2021-12-01 22:08:46 -0300

    Finishing chapter 1 of book

--- core.clj
+++ core.clj
@@ -4,7 +4,25 @@
 (defn xors [max-x max-y]
   (for [x (range max-x)
         y (range max-y)]
-    [x y (bit-xor x y)]))
+    [x y (rem (bit-xor x y) 256)]))
+
+(defn f-values [f xs ys]
+  (for [x (range xs)
+        y (range ys)]
+    [x y (rem (f x y) 256)]))
+
+(defn clear [g] (.clearRect g 0 0 200 200))
+
+(defn draw-values [frame f xs ys]
+  (.clearRect (.getGraphics frame) 0 0 xs ys)
+  (.setVisible frame true)
+  (.setSize frame (java.awt.Dimension. xs ys))
+  (let [gfx (.getGraphics frame)]
+    (doseq [[x y v] (f-values f xs ys)]
+      (.setColor gfx (java.awt.Color. v v v))
+      (.fillRect gfx x y 1 1))))
+
+(draw-values frame bit-xor 800 800)
 
 
 (def frame (java.awt.Frame.))
@@ -25,5 +43,9 @@
     (doseq [[x y xor] (xors 800 800)]
       (.setColor gfx (java.awt.Color. xor xor xor))
       (.fillRect gfx x y 1 1))))
+
+(defn clear [g] (.clearRect g 0 0 200 200))
+
+(clear gfx)
 
 (clojure.stacktrace/e)
