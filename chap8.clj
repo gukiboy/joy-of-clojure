18aa69b2897ed299e4e69a45bd856fc96d06dd96
Gustavo Kendy de Souza Kishima <gustavo.kishima@brasilparalelo.com.br>
2022-06-07 14:35:35 -0300

    More chapter8 fun

--- chap8.clj
+++ chap8.clj
@@ -72,8 +72,90 @@
 (def x 0)
 
 
+
 ;kinda tricky
 (clojure.walk/macroexpand-all `(-> {} 
                   (assoc :monke "macaco")
                   (assoc :frong 20)
                  ))
+
+;; Making the following domain
+;; Man vs monster
+;;    People
+;;      Men
+;;        Name
+;;        Have Beard? 
+;;    Monster
+;;      Chupacabra
+;;        Eat Goats?
+
+#_(domain man-vs-monster
+        (grouping people
+                  (Human "A stock human")
+                  (Man (isa Human)
+                       "A man, dude"
+                       [name]
+                       [has-beard]))
+        (grouping monster
+                  (Chupacabra
+                    "A fierce, yet elusive"
+                    [eats-goats?])))
+
+;; The above is what we want to generate using a macro
+
+
+;; below code is somewhat complex macro. You should probably study it further
+
+(defmacro domain [name & body]
+  `{:tag :domain
+    :attrs {:name (str '~name)}
+    :content [~@body]})
+
+
+(declare handle-things)
+(defmacro grouping [name & body]
+  `{:tag :grouping
+    :attrs {:name (str '~name)}
+    :content [~@(handle-things body)]})
+
+(declare grok-attrs grok-props)
+(defn handle-things [things]
+  (for [t things]
+    {:tag :thing
+     :attrs (grok-attrs (take-while (comp not vector?) t))
+     :content (if-let [c (grok-props (drop-while (comp not vector?) t))]
+                [c]
+                [])}))
+
+(defn grok-attrs [attrs]
+  (into {:name (str (first attrs))}
+        (for [a (rest attrs)]
+          (cond
+            (list? a) [:isa (str (second a))]
+            (string? a) [:comment a]))))
+
+(defn grok-props [props]
+  (when props
+    {:tag :properties :attrs nil
+     :content (apply vector (for [p props]
+                              {:tag :property
+                               :attrs {:name (str (first p))}
+                               :content nil}))}))
+
+
+
+(def dominio (domain man-vs-monster
+        (grouping people
+                  (Human "A stock human")
+                  (Man (isa Human)
+                       "A man, dude"
+                       [name]
+                       [has-beard]))
+        (grouping monster
+                  (Chupacabra
+                    "A fierce, yet elusive"
+                    [eats-goats?]))))
+
+(use '[clojure.xml :as xml])
+
+(xml/emit dominio)
